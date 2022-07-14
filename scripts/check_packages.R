99393dee51aefd953ce5850af3d5f93c5803a606
Aaron Gullickson <aarongullickson@gmail.com>
2022-07-14 12:34:45 -0700

    Set up as quarto project

---
+++ scripts/check_packages.R
@@ -0,0 +1,22 @@
+## check_packages.R
+
+#Run this script to check for packages that the other R scripts will use. If missing, try to install.
+#code borrowed from here:
+#http://www.vikram-baliga.com/blog/2015/7/19/a-hassle-free-way-to-verify-that-r-packages-are-installed-and-loaded
+
+#add new packages to the chain here
+packages = c("here","tidyverse", "broom",
+             "ggalt","scales","ggrepel","gganimate","gifski",
+             "psych","car","MASS","VGAM","nlme","nnet",
+             "maptools","sp","rgeos","rgdal",
+             "sandwich","lmtest","survey","mice",
+             "margins","logistf",
+             "gapminder",
+             "texreg", "pander", "kableExtra")
+
+package.check <- lapply(packages, FUN = function(x) {
+  if (!require(x, character.only = TRUE)) {
+    install.packages(x, dependencies = TRUE)
+    library(x, character.only = TRUE)
+  }
+})
---
+++ scripts/load_example_data.R
@@ -0,0 +1,8 @@
+load(url("https://github.com/AaronGullickson/stat_data/raw/main/output/titanic.RData"))
+load(url("https://github.com/AaronGullickson/stat_data/raw/main/output/movies.RData"))
+load(url("https://github.com/AaronGullickson/stat_data/raw/main/output/sex.RData"))
+load(url("https://github.com/AaronGullickson/stat_data/raw/main/output/popularity.RData"))
+load(url("https://github.com/AaronGullickson/stat_data/raw/main/output/earnings.RData"))
+load(url("https://github.com/AaronGullickson/stat_data/raw/main/output/crimes.RData"))
+load(url("https://github.com/AaronGullickson/stat_data/raw/main/output/politics.RData"))
+load(url("https://github.com/AaronGullickson/stat_data/raw/main/output/addhealth.RData"))
