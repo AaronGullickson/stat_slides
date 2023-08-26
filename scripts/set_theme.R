15f9573f1228d11562c08cc00a6ff96254662b18
Aaron Gullickson <aarongullickson@gmail.com>
2023-08-25 18:01:10 -0700

    Set echo to false by default and change theme

--- scripts/set_theme.R
+++ scripts/set_theme.R
@@ -3,13 +3,15 @@
 library(ggplot2)
 
 theme_myslides <- theme_bw()+
-  theme(axis.title = element_text(size=18),
-        axis.text = element_text(size=14),
-        plot.title = element_text(size=24),
-        plot.subtitle = element_text(size=16),
-        plot.caption = element_text(size=16),
-        plot.background = element_rect(fill = "#EAEBEE",
-                                       color="#EAEBEE"),
-        panel.background = element_rect(fill = "#EAEBEE"),
-        legend.background = element_rect(fill = "#EAEBEE"))
+  theme(axis.title = element_text(size=24),
+        axis.text = element_text(size=18),
+        plot.title = element_text(size=36),
+        plot.subtitle = element_text(size=30),
+        plot.caption = element_text(size=30),
+        legend.title = element_text(size=24),
+        legend.text = element_text(size=18),
+        plot.background = element_rect(fill = "white",
+                                       color="white"),
+        panel.background = element_rect(fill = "white"),
+        legend.background = element_rect(fill = "white"))
 theme_set(theme_myslides)
